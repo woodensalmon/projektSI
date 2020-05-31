4bfc197b50ca66434fe03702c469091c1acc42d9
Paul <pkrzywosz97@gmail.com>
2020-05-31 20:01:58 +0200

    test biblioteki pygad

---
+++ ga_code.py
@@ -0,0 +1,47 @@
+import pygad as g
+import bacteiria as b
+import numpy
+function_inputs = [4,-2,3.5,5,-11,-4.7]
+desired_output = 44
+
+fitness_function = b.fitness_func
+
+
+
+num_generations = 200
+num_parents_mating = 4
+
+sol_per_pop = 10
+num_genes = 1
+
+init_range_low = -2
+init_range_high = 5
+
+parent_selection_type = "sss"
+keep_parents = 1
+
+crossover_type = "single_point"
+
+mutation_type = "random"
+mutation_percent_genes = 10
+
+ga1 = g.GA(num_generations=num_generations,
+                       num_parents_mating=num_parents_mating,
+                       fitness_func=fitness_function,
+                       sol_per_pop=sol_per_pop,
+                       num_genes=num_genes,
+                       init_range_low=init_range_low,
+                       init_range_high=init_range_high,
+                       parent_selection_type=parent_selection_type,
+                       keep_parents=keep_parents,
+                       crossover_type=crossover_type,
+                       mutation_type=mutation_type,
+                       mutation_percent_genes=mutation_percent_genes
+           )
+
+ga1.run()
+solution, solution_fitness, solution_idx = ga1.best_solution()
+print("Parameters of the best solution : {solution}".format(solution=solution))
+print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+
+
